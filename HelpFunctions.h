7caebdf1e7ce922e41c69ccf7ff636dbdf1b6316
Yoanna Vasileva <61412093+yoannavslv@users.noreply.github.com>
2020-06-04 19:48:35 +0300

    Delete HelpFunctions.h

--- HelpFunctions.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-size_t numberOfSymbols(char* text, char symbol);
-char* substring(char* text, int firstPosition, int secondPosition);
-size_t index(char* text, char symbol);
-size_t theLastIndex(char* text, char symbol);
-//Vector<char*> split(char* text);
